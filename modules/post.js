44d675d15abc2a8fb8e425c1c4b2d5220c68f089
Mandeep Singhmar <msigh8328@gmail.com>
2021-04-07 17:33:02 +0530

    latest post

--- modules/post.js
+++ modules/post.js
@@ -1,34 +1,37 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
-const postSchema = new mongoose.Schema({
-  caption: {
-    type: String,
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  likes: [
-    {
-      type: ObjectId,
-      ref: "User",
+const postSchema = new mongoose.Schema(
+  {
+    caption: {
+      type: String,
+      required: true,
     },
-  ],
-  comments: [
-    {
-      text: String,
-      postedBy: {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      {
         type: ObjectId,
         ref: "User",
       },
+    ],
+    comments: [
+      {
+        text: String,
+        postedBy: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
     },
-  ],
-  postedBy: {
-    type: ObjectId,
-    ref: "User",
   },
-});
+  { timestamps: true }
+);
 
 mongoose.model("Post", postSchema);
--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,7 @@ router.get("/allpost", requireLogin, (req, res) => {
   Post.find()
     .populate("postedBy", "_id name pic")
     .populate("comments.postedBy", "_id name pic")
+    .sort('-createdAt')
     .then((posts) => {
       res.json(posts);
     })
@@ -17,9 +18,10 @@ router.get("/allpost", requireLogin, (req, res) => {
 });
 
 router.get("/getsubspost", requireLogin, (req, res) => {
-  Post.find({ postedBy: { $in: req.user.following } })
+  Post.find({ postedBy: { $in: req.user.following },req._id })
     .populate("postedBy", "_id name pic")
     .populate("comments.postedBy", "_id name pic")
+    .sort('-createdAt')
     .then((posts) => {
       res.json(posts);
     })
