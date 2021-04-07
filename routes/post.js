3b697ede9015025be929e17c58e844c7b6d9eb82
Mandeep Singhmar <msigh8328@gmail.com>
2021-04-07 17:40:32 +0530

    fixed error

--- routes/post.js
+++ routes/post.js
@@ -8,7 +8,7 @@ router.get("/allpost", requireLogin, (req, res) => {
   Post.find()
     .populate("postedBy", "_id name pic")
     .populate("comments.postedBy", "_id name pic")
-    .sort('-createdAt')
+    .sort("-createdAt")
     .then((posts) => {
       res.json(posts);
     })
@@ -18,10 +18,10 @@ router.get("/allpost", requireLogin, (req, res) => {
 });
 
 router.get("/getsubspost", requireLogin, (req, res) => {
-  Post.find({ postedBy: { $in: req.user.following },req._id })
+  Post.find({ postedBy: { $in: req.user.following } })
     .populate("postedBy", "_id name pic")
     .populate("comments.postedBy", "_id name pic")
-    .sort('-createdAt')
+    .sort("-createdAt")
     .then((posts) => {
       res.json(posts);
     })
